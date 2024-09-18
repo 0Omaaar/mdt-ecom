b5408329f67eba0efc6f312aeaf5f77f6b413bea
0Omaaar <elkhotriomarpro@gmail.com>
2024-09-18 10:30:16 +0100

    init laravel project

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+include_once 'auth.php';
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+
+//ADMIN ROUTES
+Route::get('/admin/index', function() {
+    return "ADMIN VIEW";
+})->middleware('isAdmin')->name('admin.index');
