05816a610342a8364fbf6b3b92ce82203cccb9f2
NXT-HELSING <pibo.heelsing@gmail.com>
2026-07-02 17:14:27 +0100

    fix: migration rollback error (dropForeign before dropColumn)

--- database/migrations/2024_10_16_082410_add_brand_id_to_products.php
+++ database/migrations/2024_10_16_082410_add_brand_id_to_products.php
@@ -23,6 +23,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign(['brand_id']);
             $table->dropColumn('brand_id');
         });
     }
